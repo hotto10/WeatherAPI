92f16307f61d9589014ac5ad10b38b513a9fca02
heather otto <heatherotto32@yahoo.com>
2022-11-21 15:29:56 -0600

    initial commit

---
+++ OpenWeatherMapAPI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Text.Json.Nodes;
+using static System.Net.WebRequestMethods;
+using System.IO;
+
+
+namespace OpenWeatherMapAPI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = System.IO.File.ReadAllText("appsettings.json");
+            
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+            Console.WriteLine();
+            Console.WriteLine("Enter a zipcode to check for weather");
+            var zipCode = Console.ReadLine();
+            Console.WriteLine();
+            
+
+            var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIKey}";
+
+            Console.WriteLine($"It is {WeatherMap.GetTemp(weatherURL)} degrees outside");
+        }
+    }
+}
---
+++ OpenWeatherMapAPI/WeatherMap.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace OpenWeatherMapAPI
+{
+    public class WeatherMap
+    {
+
+        public static double GetTemp(string weatherURL)
+        {
+            var client = new HttpClient();
+            var response = client.GetStringAsync(weatherURL).Result;
+            var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+            return temp;
+        }
+        
+
+    }
+}
